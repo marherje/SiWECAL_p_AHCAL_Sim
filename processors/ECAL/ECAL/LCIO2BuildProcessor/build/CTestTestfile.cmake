b4d8dbfe522615893d08adc96622ee35129deed5
Jesus Pedro Marquez Hernandez <marherje@glui01.ific.uv.es>
2024-08-05 20:56:40 +0200

    Removing an error copying files.

--- processors/ECAL/ECAL/LCIO2BuildProcessor/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /lhome/ific/m/marherje/Simplified_ECAL_PID/processors/ECAL/LCIO2BuildProcessor
-# Build directory: /lhome/ific/m/marherje/Simplified_ECAL_PID/processors/ECAL/LCIO2BuildProcessor/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
